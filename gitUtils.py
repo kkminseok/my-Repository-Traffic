4c76ebc8731b80bab05e75966702b2b96053db36
minseokkang <aaaa727@khu.ac.kr>
2022-12-28 14:15:47 +0900

    fix: remove ambiguous code

--- gitUtils.py
+++ gitUtils.py
@@ -7,5 +7,6 @@ def get_all_repositories(token: string):
     repos = g.get_repos(20,"public")
     print(repos)
     for repo in repos:
+        print(repo.full_name)
         contents = repo.get_top_paths()
         print(contents)
