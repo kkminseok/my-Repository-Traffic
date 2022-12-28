ed2fa143c338ff9d7da05cd72c4675c21e506553
minseokkang <aaaa727@khu.ac.kr>
2022-12-28 14:26:51 +0900

    fix: remove ambiguous code

--- gitUtils.py
+++ gitUtils.py
@@ -4,7 +4,7 @@ from github import Github
 
 def get_all_repositories(token: string):
     g = Github(token)
-    repos = g.get_repos("2010-11-28","public")
+    repos = g.get_user().get_repos("all")
     print(repos)
     for repo in repos:
         print(repo)
