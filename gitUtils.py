d953d1cccdec8d266f77a714778cd509527905cf
minseokkang <aaaa727@khu.ac.kr>
2022-12-28 14:03:05 +0900

    action start

--- gitUtils.py
+++ gitUtils.py
@@ -5,6 +5,7 @@ from github import Github
 def get_all_repositories(token: string):
     g = Github(token)
     repos = g.get_repos(20,"all")
+    print(repos)
     for repo in repos:
         print(repo)
         g.get_repo(repo.name)
