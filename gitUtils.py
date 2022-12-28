212316204f5cb44b2c2bc722697d9c1800d7641b
minseokkang <aaaa727@khu.ac.kr>
2022-12-28 14:54:08 +0900

    fix: remove ambiguous code

--- gitUtils.py
+++ gitUtils.py
@@ -4,9 +4,16 @@ from github import Github
 
 def get_all_repositories(token: string):
     g = Github(token)
-    repos = g.get_user().get_repos("owner")
-    print(repos)
+    repos = g.get_user().get_repos("public", "owner")
+    return repos
+
+
+def get_all_repositories_traffic(repos):
+    traffic_dict = {}
     for repo in repos:
-        print(repo)
         contents = repo.get_clones_traffic()
-        print(contents)
+        unique_cloner = contents['uniques']
+        if unique_cloner == 0:
+            continue
+        traffic_dict[repo.full_name] = contents['uniques']
+    return traffic_dict
