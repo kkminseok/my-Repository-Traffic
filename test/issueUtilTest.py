1413c0752b2a2e833fa300923c227e9f9cbbd808
kms <aaaa727@khu.ac.kr>
2023-01-02 01:13:34 +0900

    chore: some test code implement. i go to sleep..zzz

--- test/issueUtilTest.py
+++ test/issueUtilTest.py
@@ -1,19 +1,31 @@
 import pytest
-import string
 
 
 def test_compare_prev_issue(cloner: list, viewer: list, last_issue: str):
     # last_issue parsing
     last_issue_list = last_issue.split('\n')
+    prev_repo_info = {}
     for issue_info in last_issue_list:
         if issue_info.find('[') == -1:
             continue
-        repo_name = issue_info[issue_info.find('[')+1:issue_info.find(']')]
-        print(repo_name)
+        prev_repo_name = issue_info[issue_info.find('[')+1:issue_info.find(']')]
+        prev_clone_count = issue_info[issue_info.rfind(':')+1:issue_info.rfind('<')]
+        prev_repo_info[prev_repo_name] = int(prev_clone_count)
 
     for clone_data in cloner:
-        repo_name, clone_count = clone_data
-        print(repo_name,clone_count)
+        curr_repo_name, curr_clone_count = clone_data
+        print(curr_repo_name,curr_clone_count)
+        if curr_repo_name in prev_repo_info:
+            prev_count = prev_repo_info[curr_repo_name]
+            today_cloner = curr_clone_count - prev_count
+            if today_cloner > 0:
+                print("(🔼" + str(today_cloner) + ")")
+            elif today_cloner == 0:
+                print("(-)")
+            else:
+                print("(🔽" + str(today_cloner) + ")")
+
+
 
 
 
