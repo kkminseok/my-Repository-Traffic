971eb0f4be1eeeea83b6f59f33bb3181f5443436
kms <aaaa727@khu.ac.kr>
2023-01-04 21:07:41 +0900

    feat: viewer 증감 상태 추가, new 이모지 변경

--- issueUtil.py
+++ issueUtil.py
@@ -5,6 +5,7 @@ def create_issue_content(cloner_data: list, view_data: list, last_issue_body: st
     # 이전 이슈와 비교
     compare_result = compare_prev_issue(cloner_data, view_data, last_issue_body)
     prev_clone_dict = compare_result[0]
+    prev_view_dict = compare_result[1]
     issue_cloner_header = '## Unique Cloner <br/> \n'
     issue_viewer_header = '## Unique viewer <br/> \n'
     issue_list.append(issue_cloner_header)
@@ -21,7 +22,8 @@ def create_issue_content(cloner_data: list, view_data: list, last_issue_body: st
 
     for unique_view in view_data:
         repo_name, viewer = unique_view
-        issue_list.append(f"- [{repo_name}]({github_url}" + repo_name + f") 의 방문자:{viewer} <br/>\n")
+        viewer_update = prev_view_dict[repo_name]
+        issue_list.append(f"- [{repo_name}]({github_url}" + repo_name + f") 의 방문자:{viewer} : {viewer_update} <br/>\n")
 
     issue_list.append("If you, the creator, also visit or clone the repository daily, the results will be counted and "
                       "accumulated daily. Please be aware of this.<br/>")
@@ -32,12 +34,11 @@ def create_issue_content(cloner_data: list, view_data: list, last_issue_body: st
 def compare_prev_issue(current_cloner: list, current_view: list, last_issue: str) -> list:
     prev_cloner = get_prev_cloner(last_issue)
     prev_viewer = get_prev_viewer(last_issue)
-    # TODO viewer도
     compare_result = []
     cloner_compare = compare_prev_cloner(prev_cloner, current_cloner)
     compare_result.append(cloner_compare)
+    compare_result.append(prev_viewer)
     return compare_result
-
 
 
 def get_prev_cloner(last_issue: str) -> dict:
@@ -72,7 +73,6 @@ def compare_prev_cloner(prev_cloner, current_cloner) -> dict:
         curr_repo_name, curr_clone_count = curr_cloner_data
         if curr_repo_name in prev_cloner:
             prev_count = prev_cloner[curr_repo_name]
-            print("prev count:", prev_count, "current count:", curr_clone_count)
             cloner_status = ""
             today_cloner = curr_clone_count - prev_count
             if today_cloner > 0:
@@ -82,6 +82,26 @@ def compare_prev_cloner(prev_cloner, current_cloner) -> dict:
             else:
                 cloner_status = "(🔽" + str(today_cloner) + ")"
         else:
-            cloner_status = "new!!"
+            cloner_status = "🔅 new"
         compare_result[curr_repo_name] = cloner_status
+    return compare_result
+
+
+def compare_prev_viewer(prev_viewer, current_viewer) -> dict:
+    compare_result = {}
+    for curr_cloner_data in current_viewer:
+        curr_repo_name, curr_view_count = curr_cloner_data
+        if curr_repo_name in prev_viewer:
+            prev_count = prev_viewer[curr_repo_name]
+            viewer_status = ""
+            today_cloner = curr_view_count - prev_count
+            if today_cloner > 0:
+                viewer_status = "(🔼" + str(today_cloner) + ")"
+            elif today_cloner == 0:
+                viewer_status = "(-)"
+            else:
+                viewer_status = "(🔽" + str(today_cloner) + ")"
+        else:
+            viewer_status = "🔅 new"
+        compare_result[curr_repo_name] = viewer_status
     return compare_result
